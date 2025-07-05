9b4e8c2e57182896485f3a544be42cb321cdd256
Дмитрий Бурмак <97616159+DmitriyBurmak@users.noreply.github.com>
2025-07-05 20:25:26 +0300

    Adding Meta tags and Open Graph for app\notes\filter\[...slug]\page.tsx

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -6,9 +6,49 @@ import {
 } from '@tanstack/react-query';
 import NotesClient from './Notes.client';
 import type { NotesResponse } from '@/types/note';
+import { Metadata } from 'next';
+import { title } from 'process';
 
 interface NotesPageProps {
   params: Promise<{ slug: string[] }>;
+}
+export async function generateMetadata({
+  params,
+}: NotesPageProps): Promise<Metadata> {
+  const { slug } = await params;
+  const tag = slug?.[0] || 'all';
+
+  const pageTitle =
+    tag === 'all'
+      ? 'NoteHub - Усі нотатки'
+      : `NoteHub - Нотатки за тегом: ${tag}`;
+  const pageDescription =
+    tag === 'all'
+      ? 'Переглядайте та керуйте всіма своїми нотатками на NoteHub.'
+      : `Переглядайте та керуйте своїми нотатками, відфільтрованими за тегом "${tag}" на NoteHub.`;
+
+  const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : 'http://localhost:3000';
+  const pageUrl = `${baseUrl}/notes/filter/${tag.toLowerCase()}`;
+
+  return {
+    title: pageTitle,
+    description: pageDescription,
+    openGraph: {
+      title: pageTitle,
+      description: pageDescription,
+      url: pageUrl,
+      images: [
+        {
+          url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+          width: 1200,
+          height: 630,
+          alt: `NoteHub - ${tag === 'all' ? 'All notes' : `${tag} notes`} page`,
+        },
+      ],
+    },
+  };
 }
 
 export default async function NotesPage({ params }: NotesPageProps) {
