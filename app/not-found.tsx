0e27fd8452c3a741555aa25bc289e65082fb5a2a
Дмитрий Бурмак <97616159+DmitriyBurmak@users.noreply.github.com>
2025-07-05 18:11:48 +0300

    Adding Meta tags and Open Graph for not-found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,24 @@
 import Link from 'next/link';
 import css from './not-found.module.css';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '404 - Page not found',
+  description: 'Sorry, the page you are looking for does not exist.',
+  openGraph: {
+    title: '',
+    description: '',
+    url: 'https://www.edu.goit.global/ru/learn/29773997/38700675/39972039/training?blockId=41035403',
+    images: [
+      {
+        url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Page not found',
+      },
+    ],
+  },
+};
 
 const NotFound = () => {
   return (
