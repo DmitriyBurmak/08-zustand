7452d272600fee4ec04d1a3ede46d5934bdc3545
Дмитрий Бурмак <97616159+DmitriyBurmak@users.noreply.github.com>
2025-07-06 18:06:12 +0300

    improving the app/layout.tsx file

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,13 +13,15 @@ const roboto = Roboto({
 });
 
 export const metadata: Metadata = {
-  title: 'Notes',
+  title: 'NoteHub - Усі нотатки',
   description:
     'Notes is a simple and fast application for creating, searching, and saving notes. Everything is at your fingertips when you need it.',
   openGraph: {
     title: 'NoteHub - Усі нотатки',
     description: 'Переглядайте та керуйте всіма своїми нотатками на NoteHub.',
-    url: 'https://www.edu.goit.global/ru/learn/29773997/38700675/39972039/training?blockId=41035403',
+    url: process.env.NEXT_PUBLIC_VERCEL_URL
+      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+      : 'http://localhost:3000',
     images: [
       {
         url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
